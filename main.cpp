da6773910b8bd409a67db94c915d3ea2d18c41ee
Leonardo-Lazzara <91598283+Leonardo-Lazzara@users.noreply.github.com>
2021-11-30 09:21:31 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int n;
+    cin >> n;
+    if(n==1){
+        cout << "numero non primo" << endl;
+    } else if (numeri_primi(n, n/2)){
+        cout << "numero primo" << endl;
+    }else{
+        cout << "numero non primo" << endl;
+    }
+    return 0;
 }
