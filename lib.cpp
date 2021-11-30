03984919a4089cca849afac31e6405aab9030345
Leonardo-Lazzara <91598283+Leonardo-Lazzara@users.noreply.github.com>
2021-11-30 09:20:52 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
+#include "lib.h"
+#include <iostream>
 
+bool numeri_primi(int a, int b){
+    if(b==1){
+        return true;
+    }else if(a%b==0) {
+        return false;
+    }else{
+        return numeri_primi(a, b-1);
+    }
+}
